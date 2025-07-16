fe21cea708b3853f3c3a516324f43547feeec377
Rouf -Myz <rouf08412@gmail.com>
2025-07-16 09:43:18 +0700

    Update close database connection

--- test-connection.php
+++ test-connection.php
@@ -11,6 +11,9 @@ $password = "";
 try {
     $connection = new PDO("mysql::host=$host:$port;dbname=$database", $username, $password);
     echo "Sukses Terkoneksi ke database MySQL" . PHP_EOL;
+
+    // menutup koneksi database 
+    $connection = null;
 } catch (PDOException $exception) {
     echo "gagal terkoneksi ke database MySQL : " . $exception->getMessage() . PHP_EOL;
 }
