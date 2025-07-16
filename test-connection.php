82e59064df0094a56489657b2a41a370a51f6ca3
Rouf -Myz <rouf08412@gmail.com>
2025-07-16 09:39:25 +0700

    Commit Learn PHP MySQL

---
+++ test-connection.php
@@ -0,0 +1,17 @@
+<?php
+
+$host = "localhost";
+$port = "3306";
+$database = "bigdata_v1";
+$username = "root";
+$password = "";
+
+// cara koneksi ke database menggunakan php
+
+try {
+    $connection = new PDO("mysql::host=$host:$port;dbname=$database", $username, $password);
+    echo "Sukses Terkoneksi ke database MySQL" . PHP_EOL;
+} catch (PDOException $exception) {
+    echo "gagal terkoneksi ke database MySQL : " . $exception->getMessage() . PHP_EOL;
+}
+
